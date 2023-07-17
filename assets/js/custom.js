55bd277efbb030f4b66658ef4bd86a2636a24eb3
Mahmoud <oudy_mosa@hotmail.com>
2023-07-17 13:50:50 +0300

    news-list + profile-pics + custom.js edit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,5 @@
 $( document ).ready(function() {
 
-    if($('.InHome').length ){
-        $('.homeBanner').addClass('homeClass')
-     }
-    $(".navbar-light .dmenu").hover(
-        function () {
-          $(this).find(".sm-menu").first().stop(true, true).slideDown(150);
-        },
-        function () {
-          $(this).find(".sm-menu").first().stop(true, true).slideUp(105);
-        }
-      );
-
     let arrow = [
       '<img src="../assets/images/home/Arrow-Right.svg" />',
       '<img src="../assets/images/home/Arrow-Left.svg" />',
@@ -136,7 +124,7 @@ $('.owl-carousel-vote').owlCarousel({
 
 
 
-    // end ready
+
 
 
 
@@ -165,78 +153,103 @@ $('.owl-carousel-vote').owlCarousel({
   });
 
 
-});
 
 
 
 
-    /* --- Font sizing Function --- */
-    // $('.top-functions .increase').click(function(){
-    //     console.log("increase");
-    //     modifyFontSize('html','increase');
-    //   });
-    //   $('.top-functions .decrease').click(function(){
-    //     console.log("dec");
-    //     modifyFontSize('html','decrease')
-    //   });
-
-        /*----Font sizing Function ---*/
-  // function modifyFontSize(MyElement,flag){
-
-  //   var HtmlElement = $(MyElement);
-  //   var currentFontSize = parseInt (HtmlElement.css('font-size'));
-    
-  //   if (flag =='increase' & currentFontSize < 19 )
-  //       currentFontSize += 1;
-  //   else if (flag == 'decrease' & currentFontSize >= 16 )
-  //       currentFontSize -= 1;
-  //   else if (flag == 'reset')
-  //   currentFontSize = 16;
-
-  //   HtmlElement.css('font-size', currentFontSize);
-
-  //   // console.log(currentFontSize);
-
-  // }
-
-
-
-
-
-  // $(".topNav .slide-nav").on("click", function (e) {
-  //   console.log("slideeeeeeeee");
-  //   $(".bannerBg").toggleClass("slide-me");
-  //   e.stopPropagation();
-   
-  // });
-
-
-
-
-
-
-  // $(".social-share ul li ").on("hover", function (e) {
-  //   console.log("hover");
-  //   $(this).toggleClass("hoverd");
-  //   e.stopPropagation();
-   
-  // });
 
   $(".social-share ul li").hover(function () {
     console.log("hover");
     $(this).toggleClass("hoverd");
-});
-
-$(".sub-menu-parent").on("click", function (e) {
+  });
+  
+  $(".sub-menu-parent").on("click", function (e) {
   console.log("sub-childssssss");
   $(this).parent().toggleClass("opened");
+  e.stopPropagation();
+  
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ AO start 
+*/
+
+  function animateElements() {
+    $('.progressbarCircle').each(function () {
+        let elementPos = $(this).offset().top;
+        let topOfWindow = $(window).scrollTop();
+        // var percent = $(this).find('.circle').attr('data-percent');
+        let minValue = $(this).find('.circle').attr('aria-valuenow');
+        let maxValue = $(this).find('.circle').attr('aria-valuemax');
+        let animate = $(this).data('animate');
+        let total = (minValue / maxValue) * 100
+  
+        if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+            $(this).data('animate', true);
+            $(this).find('.circle').circleProgress({
+                startAngle: -Math.PI / 1.3,
+                value: total / 100,
+                size : 92,
+                thickness:10,
+                emptyFill: " rgba(0, 112, 103, 0.99)" ,
+                fill: {
+                  color: "rgba(204, 160, 121, 0.99)"
+                }
+            }).on('circle-animation-progress', function (event, progress, stepValue) {
+                $(this).find('strong').text((stepValue*100).toFixed(0));
+                // instance.arcFill = setGradient(stepValue)
+            }).stop();
+        }
+    });
+}
+
+animateElements();
+// $(window).scroll(animateElements);
+
+if($('body').hasClass("Dark")) {
+  // $('.colorTwo').circleProgress({ emptyFill:"rgba(187, 187, 187, 0.99)", fill: { color: '#f0f' }});
+}else {
+  $('.colorTwo').circleProgress({  emptyFill:"rgba(204, 160, 121, 0.99)", fill: { color: ' rgba(0, 112, 103, 0.99)' }});
+}
+
+$(".dark-Mode").on("click", function (e) {
+  console.log("dark-Mode");
+  $(this).toggleClass("dark-here");
+  $("body").toggleClass("Dark");
+  if($('body').hasClass("Dark")) {
+    $('.circle').circleProgress({ emptyFill:"rgba(0, 154, 189, 0.99)", fill: { color: 'rgba(187, 187, 187, 0.99)' }});
+    $('.colorTwo').circleProgress({ emptyFill:"rgba(187, 187, 187, 0.99)", fill: { color: 'rgba(0, 112, 103, 0.99)' }});
+  }else {
+    $('.circle').circleProgress({  emptyFill:"rgba(0, 112, 103, 0.99)", fill: { color: ' rgba(204, 160, 121, 0.99)' }});
+    $('.colorTwo').circleProgress({  emptyFill:"rgba(204, 160, 121, 0.99)", fill: { color: ' rgba(0, 112, 103, 0.99)' }});
+  }
   e.stopPropagation();
  
 });
 
 
 
+});
 
+    // end ready
+
+
+
+
+
+
+// swiper
 
 $(function () {
 
@@ -300,62 +313,11 @@ $(function () {
  AO start 
 */
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    function animateElements() {
-        $('.progressbarCircle').each(function () {
-            let elementPos = $(this).offset().top;
-            let topOfWindow = $(window).scrollTop();
-            // var percent = $(this).find('.circle').attr('data-percent');
-            let minValue = $(this).find('.circle').attr('aria-valuenow');
-            let maxValue = $(this).find('.circle').attr('aria-valuemax');
-            let animate = $(this).data('animate');
-            let total = (minValue / maxValue) * 100
-      
-            if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
-                $(this).data('animate', true);
-                $(this).find('.circle').circleProgress({
-                    startAngle: -Math.PI / 1.3,
-                    value: total / 100,
-                    size : 92,
-                    thickness:10,
-                    emptyFill: " rgba(0, 112, 103, 0.99)" ,
-                    fill: {
-                      color: "rgba(204, 160, 121, 0.99)"
-                    }
-                }).on('circle-animation-progress', function (event, progress, stepValue) {
-                    $(this).find('strong').text((stepValue*100).toFixed(0));
-                    // instance.arcFill = setGradient(stepValue)
-                }).stop();
-            }
-        });
-    }
-
-    animateElements();
-    // $(window).scroll(animateElements);
-
-    if($('body').hasClass("Dark")) {
-      // $('.colorTwo').circleProgress({ emptyFill:"rgba(187, 187, 187, 0.99)", fill: { color: '#f0f' }});
-    }else {
-      $('.colorTwo').circleProgress({  emptyFill:"rgba(204, 160, 121, 0.99)", fill: { color: ' rgba(0, 112, 103, 0.99)' }});
-    }
-
-    $(".dark-Mode").on("click", function (e) {
-      console.log("dark-Mode");
-      $(this).toggleClass("dark-here");
-      $("body").toggleClass("Dark");
-      if($('body').hasClass("Dark")) {
-        $('.circle').circleProgress({ emptyFill:"rgba(0, 154, 189, 0.99)", fill: { color: 'rgba(187, 187, 187, 0.99)' }});
-        $('.colorTwo').circleProgress({ emptyFill:"rgba(187, 187, 187, 0.99)", fill: { color: 'rgba(0, 112, 103, 0.99)' }});
-      }else {
-        $('.circle').circleProgress({  emptyFill:"rgba(0, 112, 103, 0.99)", fill: { color: ' rgba(204, 160, 121, 0.99)' }});
-        $('.colorTwo').circleProgress({  emptyFill:"rgba(204, 160, 121, 0.99)", fill: { color: ' rgba(0, 112, 103, 0.99)' }});
-      }
-      e.stopPropagation();
-     
-    });
+   
     
-});
+// });
 
 
 
